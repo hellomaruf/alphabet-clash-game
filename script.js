d4b3bc59966984145db4135a04db3322ad549dce
maruf-ahmed <marufahamed1110@gmail.com>
2024-02-14 20:30:30 +0600

    finished score section with some js interaction

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// function play() {
+//   let homeSection = document.getElementById("home-section");
+//   homeSection.classList.add("hidden");
+//   let playGroundSection = document.getElementById("play-ground-section");
+//   playGroundSection.classList.remove("hidden");
+// }
 function play() {
-    console.log("Home page");
+  removeElementById("home-section");
+  showElementById("play-ground-section");
+  continueGame();
+}
+
+function continueGame() {
+  let alphabet = getARandomAlphabet();
+  let currentAlphabet = document.getElementById("current-alphabet");
+  currentAlphabet.innerText = alphabet;
+  setBackgroundById(alphabet);
 }
---
+++ utility.js
@@ -0,0 +1,32 @@
+// Function for remove element by id
+function removeElementById(elementId) {
+  let element = document.getElementById(elementId);
+  element.classList.add("hidden");
+}
+
+// Function for show element by id
+function showElementById(elementId) {
+  let element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+
+// Get Random Alphabet
+function getARandomAlphabet() {
+  // get alphabet from a string
+  let alphabetString = "qwertyuiopasdfghjklzxcvbnm";
+  let alphabets = alphabetString.split("");
+
+  //   console.log(alphabets);
+
+  // generate random number
+  let randomNumber = Math.random() * 25;
+  let index = Math.round(randomNumber);
+  let alphabet = alphabets[index];
+//   let alphabetUpper = alphabet.toUpperCase();
+  return alphabet;
+}
+
+function setBackgroundById(elementId) {
+    let element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400')
+}
